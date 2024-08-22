faf0a51d7733a11304bb1170468f83eb74d6795b
Olafcio <olafcio@int.pl>
2024-08-22 15:26:30 +0200

    1st thing

---
+++ assets/all.js
@@ -0,0 +1,11 @@
+(async () => {
+    // kod hujowy pewnie ale shut the fuck up bby
+    // ~olafciooox
+    const html = document.documentElement;
+    addEventListener("scroll", ev => {
+        if (ev.target == document) {
+            html.classList.toggle("scrolled", !!html.scrollTop);
+            html.style.setProperty("--scrolltop", html.scrollTop + "px");
+        }
+    }, true);
+})();
